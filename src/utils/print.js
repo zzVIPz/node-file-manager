fc26c207c2365ce7fe48c094633f7b247d5310de
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-02-03 20:34:28 -0500

    implement sign out logic

---
+++ src/utils/print.js
@@ -0,0 +1,15 @@
+export const print = (text, foregroundColor) => {
+  let colorCode = '';
+  switch (foregroundColor) {
+    case 'red':
+      colorCode = `\x1b[31m`;
+      break;
+    case 'green':
+      colorCode = `\x1b[32m`;
+      break;
+    case 'blue':
+      colorCode = `\x1b[34m`;
+  }
+
+  console.log(`${colorCode}${text}\x1b[0m`);
+};
